40b9f6eee04730b5d16b511c6784b90c1ca8baa9
siciliano-diag <siciliano@diag.uniroma1.it>
2023-09-10 18:25:18 +0200

    torchmetrics won...

--- torch_utils/metrics.py
+++ torch_utils/metrics.py
@@ -1,5 +1,6 @@
 import torch.nn.functional as F
 import torchmetrics
+import torch
 
 def nn_accuracy(y_hat,y):
     soft_y_hat = F.softmax(y_hat).argmax(dim=-1)
@@ -8,15 +9,28 @@ def nn_accuracy(y_hat,y):
 
     return acc
 
-#Custom Accuracy to compute accuracy with Soft Labels
-class SoftLabelsAccuracy(torchmetrics.Accuracy):
-    def init(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        #self.__name__ = "custom_accuracy"
-        print("NEW ACCURACY")
+#Custom Accuracy to compute accuracy with Soft Labels as torch.Module
+class SoftLabelsAccuracy(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
 
-    def forward(self, y_hat, y):
-        hard_y = y.argmax(dim=-1)
-        print(hard_y)
-        acc = super().forward(y_hat, hard_y)
-        return acc
+    def forward(self, preds: torch.Tensor, target: torch.Tensor):
+        return (preds.argmax(dim=1) == target.argmax(dim=1)).float().mean()
+
+
+#Custom Accuracy to compute accuracy with Soft Labels
+class SoftLabelsAccuracy2(torchmetrics.Metric):
+    def __init__(self):
+        super().__init__()
+        self.add_state("correct", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.add_state("total", default=torch.tensor(0), dist_reduce_fx="sum")
+
+    def update(self, preds: torch.Tensor, target: torch.Tensor):
+        #preds, target = self._input_format(preds, target)
+        #assert preds.shape == target.shape
+
+        self.correct += torch.sum(preds.argmax(dim=1) == target.argmax(dim=1))
+        self.total += target.shape[0]
+
+    def compute(self):
+        return self.correct.float() / self.total
